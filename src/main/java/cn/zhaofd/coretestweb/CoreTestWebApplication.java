dd51e326ebd25646e4b2e2abbd193c0cc257e1f6
Tobe-Wang <hzbeq@qq.com>
2025-06-10 16:40:00 +0800

    feat(coretestweb): 添加 CoreTestWebApplication 启动类
    - 新增 CoreTestWebApplication 类作为 Spring Boot 应用程序的入口
    - 使用 @SpringBootApplication 注解配置应用程序
    - 实现 main 方法，启动 Spring Boot 应用程序

---
+++ src/main/java/cn/zhaofd/coretestweb/CoreTestWebApplication.java
@@ -0,0 +1,11 @@
+package cn.zhaofd.coretestweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreTestWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CoreTestWebApplication.class, args);
+    }
+}
