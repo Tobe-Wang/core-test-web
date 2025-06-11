b0d2ae44d7af63c3516133e94ddec0992557c0d3
Tobe-Wang <hzbeq@qq.com>
2025-06-11 11:04:28 +0800

    feat(rest): 完善 RESTful 接口功能
    - 新增根据 ID 获取对象的接口
    - 新增获取列表的接口
    - 新增保存对象的接口
    - 新增修改对象的接口
    - 新增删除对象的接口
    - 优化现有接口的实现，提高代码复用性

--- src/main/java/cn/zhaofd/coretestweb/rest/web/RestfulController.java
+++ src/main/java/cn/zhaofd/coretestweb/rest/web/RestfulController.java
@@ -4,15 +4,26 @@
 
 package cn.zhaofd.coretestweb.rest.web;
 
+import cn.zhaofd.core.base.ObjectUtil;
 import cn.zhaofd.coretestweb.rest.dto.Customer;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/rest")
 public class RestfulController {
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public Customer getObj(@RequestParam Integer id) {
+    /**
+     * 获取单个对象
+     *
+     * @param id 主键
+     * @return Customer
+     */
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Customer getById(@PathVariable Integer id) {
         Customer customer = new Customer();
         customer.setId(id);
         customer.setFirstName("zhao");
@@ -20,20 +31,65 @@ public class RestfulController {
         return customer;
     }
 
+    /**
+     * 获取列表
+     *
+     * @param params 查询参数
+     * @return 列表
+     */
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Customer> getList(@RequestParam Map<String, Object> params) {
+        Integer id = ObjectUtil.convert(params.get("id"), Integer.class);
+        String firstName = ObjectUtil.convert(params.get("firstName"), String.class);
+        String lastName = ObjectUtil.convert(params.get("lastName"), String.class);
+
+        List<Customer> list = new ArrayList<>();
+        Customer customer1 = new Customer();
+        customer1.setId(id);
+        customer1.setFirstName(firstName);
+        customer1.setLastName(lastName);
+        list.add(customer1);
+        Customer customer2 = new Customer();
+        customer2.setId(2);
+        customer2.setFirstName("zhao");
+        customer2.setLastName("fang dong");
+        list.add(customer2);
+
+        return list;
+    }
+
+    /**
+     * 保存对象
+     *
+     * @param customer 保存对象
+     * @return 保存后对象
+     */
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Customer postObj(@RequestBody Customer customer) {
+    public Customer postEntity(@RequestBody Customer customer) {
         customer.setFirstName("保存成功");
         return customer;
     }
 
+    /**
+     * 修改对象
+     *
+     * @param customer 修改对象
+     * @return 修改后对象
+     */
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Customer putObj(@RequestBody Customer customer) {
+    public Customer putEntity(@RequestBody Customer customer) {
         customer.setFirstName("修改成功");
         return customer;
     }
 
+    /**
+     * 删除对象
+     *
+     * @param id 主键
+     * @return 删除数量
+     */
     @DeleteMapping(value = "/{id}")
-    public String deleteObj(@PathVariable Integer id) {
-        return "删除成功";
+    public Integer deleteById(@PathVariable Integer id) {
+        return 1;
     }
 }
