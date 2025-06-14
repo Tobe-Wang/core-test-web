a283d3c6c75aa969bf09709ebc79b00358641b70
Tobe-Wang <hzbeq@qq.com>
2025-06-14 13:09:46 +0800

    feat(coretestweb): 添加全局异常处理器
    - 新增 GlobalExceptionHandler 类用于统一处理异常
    - 实现了对 HttpException 的处理，返回自定义错误信息和状态码

---
+++ src/main/java/cn/zhaofd/coretestweb/advice/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2025. Tobe Wang
+ */
+
+package cn.zhaofd.coretestweb.advice;
+
+import cn.zhaofd.coretestweb.core.exception.HttpException;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    /**
+     * 统一处理HttpException
+     *
+     * @param e HttpException
+     * @return ResponseEntity 响应
+     */
+    @ExceptionHandler(HttpException.class)
+    public ResponseEntity<String> handleHttpException(HttpException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatusCode.valueOf(e.getStatusCode()));
+    }
+}
