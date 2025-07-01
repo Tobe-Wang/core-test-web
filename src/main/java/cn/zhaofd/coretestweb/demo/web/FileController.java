bc29462f64421d818df751d581f314e85232bf98
Tobe-Wang <hzbeq@qq.com>
2025-07-01 14:38:14 +0800

    feat(coretestweb): 添加文件上传下载功能
    - 新增 FileController 类实现文件上传和下载功能- 使用 RestFileUtil 工具类进行文件操作
    - 上传文件时验证输入参数，确保文件不为空
    - 下载文件时验证输入参数，确保文件名不为空
    - 根据配置文件路径进行文件上传和下载

---
+++ src/main/java/cn/zhaofd/coretestweb/demo/web/FileController.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2025. Tobe Wang
+ */
+
+package cn.zhaofd.coretestweb.demo.web;
+
+import cn.zhaofd.core.base.StringUtil;
+import cn.zhaofd.core.spring.web.RestFileUtil;
+import cn.zhaofd.coretestweb.config.PropertyConfig;
+import cn.zhaofd.coretestweb.core.exception.HttpException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * 文件上传下载Controller
+ */
+@RestController
+@RequestMapping("/file")
+public class FileController {
+    private final PropertyConfig propertyConfig;
+
+    @Autowired
+    public FileController(PropertyConfig propertyConfig) {
+        this.propertyConfig = propertyConfig;
+    }
+
+    /**
+     * 上传文件
+     *
+     * @param file 文件
+     * @param request HttpServletRequest对象
+     * @return 状态码
+     */
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public String upload(@RequestParam MultipartFile file, HttpServletRequest request) {
+        // 输入参数验证
+        if (file == null) {
+            throw new HttpException(HttpStatus.BAD_REQUEST.value(), "文件为空");
+        }
+
+        /*
+        // 上传文件(使用应用相对目录保存)
+        String dirPath = RestFileUtil.getRealPath(request, propertyConfig.getValue("server.upload.relativeDir"));
+        return RestFileUtil.upload(file, dirPath);
+        */
+
+        return RestFileUtil.upload(file, propertyConfig.getValue("server.upload.dirPath"));
+    }
+
+    /**
+     * 下载文件
+     *
+     * @param serverFileName 服务端文件名
+     * @param downloadFileName 下载文件名
+     * @return 文件流
+     */
+    @GetMapping(value = "/download")
+    public ResponseEntity<byte[]> download(@RequestParam String serverFileName, @RequestParam String downloadFileName) {
+        // 输入参数验证
+        if (StringUtil.isNullOrTrimEmpty(serverFileName)) {
+            throw new HttpException(HttpStatus.BAD_REQUEST.value(), "参数serverFileName不能为空");
+        }
+        if (StringUtil.isNullOrTrimEmpty(downloadFileName)) {
+            throw new HttpException(HttpStatus.BAD_REQUEST.value(), "参数downloadFileName不能为空");
+        }
+
+        Path filePath = Paths.get(propertyConfig.getValue("server.upload.dirPath"), serverFileName);
+        return RestFileUtil.download(filePath.toAbsolutePath().toString(), downloadFileName);
+    }
+}
