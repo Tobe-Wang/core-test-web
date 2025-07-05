4c1c65649044532270edc29fbbdc5409ed553149
Tobe-Wang <hzbeq@qq.com>
2025-07-05 20:47:29 +0800

    feat(coretestweb): 添加 SpringMvc 配置类
    - 新增 SpringMvcConfig 类，实现 WebMvcConfigurer 接口
    - 添加请求拦截器配置，使用 RequestInterceptor拦截请求
    - 通过 PropertyConfig 获取配置参数，设置请求拦截器的警告阈值

---
+++ src/main/java/cn/zhaofd/coretestweb/config/SpringMvcConfig.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2025. Tobe Wang
+ */
+
+package cn.zhaofd.coretestweb.config;
+
+import cn.zhaofd.core.base.ObjectUtil;
+import cn.zhaofd.coretestweb.config.interceptor.RequestInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * SpringMvc配置
+ */
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+    private final PropertyConfig propertyConfig;
+
+    @Autowired
+    public SpringMvcConfig(PropertyConfig propertyConfig) {
+        this.propertyConfig = propertyConfig;
+    }
+
+    /**
+     * 拦截器配置
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {// 2
+        registry.addInterceptor(requestInterceptor());
+    }
+
+    /**
+     * RESTFul请求拦截器Bean
+     */
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        // RESTFul请求超时的警告日志阈值(毫秒)
+        Long warnThreshold = ObjectUtil.convert(propertyConfig.getValue("server.request.timeout.warnThreshold"), Long.class);
+
+        return new RequestInterceptor(warnThreshold);
+    }
+}
