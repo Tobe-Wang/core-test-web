8e63c60538d27f5d63abc322d4cfee500150de59
Tobe-Wang <hzbeq@qq.com>
2025-07-07 13:31:24 +0800

    build(maven): 添加解决 Spring Boot 测试中 Mockito 警告的配置
    - 引入 maven-surefire-plugin 插件
    - 配置插件版本为 3.5.3- 在插件配置中添加 argLine 参数，解决 Spring Boot 测试中的 Mockito 警告问题

---
+++ src/test/java/cn/zhaofd/coretestweb/demo/web/RestfulControllerTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2025. Tobe Wang
+ */
+
+package cn.zhaofd.coretestweb.demo.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class RestfulControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getById() throws Exception {
+        // @formatter:off
+        mockMvc.perform(get("/rest/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(content().json("{\"id\":1,\"firstName\":\"zhao王\",\"lastName\":\"fang dong\"}"));
+        // @formatter:on
+    }
+
+    @Test
+    void deleteById() throws Exception {
+        // @formatter:off
+        mockMvc.perform(delete("/rest/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("1"));
+        // @formatter:on
+    }
+}
