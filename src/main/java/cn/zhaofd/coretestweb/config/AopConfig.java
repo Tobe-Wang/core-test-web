09dc2b26edb18620bd7d484699823bdffcd5c856
Tobe-Wang <hzbeq@qq.com>
2025-07-08 22:23:10 +0800

    feat(coretestweb): 添加 AOP 配置类
    - 新增 AopConfig 类，启用 AspectJ 自动代理
    - 位于 cn.zhaofd.coretestweb.config 包中

---
+++ src/main/java/cn/zhaofd/coretestweb/config/AopConfig.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2025. Tobe Wang
+ */
+
+package cn.zhaofd.coretestweb.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * AOP的AspectJ自动代理配置
+ */
+@Configuration
+@EnableAspectJAutoProxy
+public class AopConfig {
+}
