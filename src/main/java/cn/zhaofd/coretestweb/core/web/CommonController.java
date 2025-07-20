a58725753c1771457f4e7038037b99a37911c6d6
Tobe-Wang <hzbeq@qq.com>
2025-07-20 10:02:59 +0800

    feat(coretestweb): 添加 CommonController 用于处理公共方法

---
+++ src/main/java/cn/zhaofd/coretestweb/core/web/CommonController.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2025. Tobe Wang
+ */
+
+package cn.zhaofd.coretestweb.core.web;
+
+import cn.zhaofd.core.spring.web.web.BaseCommonController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 公共方法的Controller
+ */
+@RestController
+@RequestMapping("/common")
+public class CommonController extends BaseCommonController {
+}
