923722b14596f730637a3baa2ca5e759979dbf52
Tobe-Wang <hzbeq@qq.com>
2025-07-24 14:19:29 +0800

    refactor(exception): 重构全局异常处理
    - 删除了自定义的 GlobalExceptionHandler 类
    - 继承了基类 BaseGlobalExceptionHandler
    - 移除了处理 HttpException 的代码，改为使用基类的统一异常处理逻辑

--- src/main/java/cn/zhaofd/coretestweb/config/advice/GlobalExceptionHandler.java
+++ src/main/java/cn/zhaofd/coretestweb/config/advice/GlobalExceptionHandler.java
@@ -4,25 +4,12 @@
 
 package cn.zhaofd.coretestweb.config.advice;
 
-import cn.zhaofd.core.net.exception.HttpException;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import cn.zhaofd.core.spring.web.config.advice.BaseGlobalExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
  * 全局Controller异常处理
  */
 @RestControllerAdvice
-public class GlobalExceptionHandler {
-    /**
-     * 统一处理HttpException
-     *
-     * @param e HttpException
-     * @return ResponseEntity 响应
-     */
-    @ExceptionHandler(HttpException.class)
-    public ResponseEntity<String> handleHttpException(HttpException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatusCode.valueOf(e.getStatusCode()));
-    }
+public class GlobalExceptionHandler extends BaseGlobalExceptionHandler {
 }
