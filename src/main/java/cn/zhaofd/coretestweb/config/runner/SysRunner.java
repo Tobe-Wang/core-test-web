a95d0c715c0c44cf679b8995c4a88eb4f0d52c1f
Tobe-Wang <hzbeq@qq.com>
2025-07-29 13:40:58 +0800

    feat(coretestweb): 添加系统启动时执行的 SysRunner 组件- 新增 SysRunner 类，实现 CommandLineRunner 接口 - 在系统启动时记录日志信息- 通过 @Component 和 @Order 注解实现组件注册和执行顺序设置

---
+++ src/main/java/cn/zhaofd/coretestweb/config/runner/SysRunner.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2025. Tobe Wang
+ */
+
+package cn.zhaofd.coretestweb.config.runner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * 系统启动时执行
+ */
+@Component
+@Order(1)
+public class SysRunner implements CommandLineRunner {
+    private static final Logger logger = LoggerFactory.getLogger(SysRunner.class);
+
+    /**
+     * 启动时执行
+     *
+     * @param args 启动参数
+     */
+    @Override
+    public void run(String... args) {
+        logger.info("程序启动后执行的代码");
+    }
+}
