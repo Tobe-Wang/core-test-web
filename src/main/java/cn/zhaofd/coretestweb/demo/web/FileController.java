061f6d8fda5fff6bf6fc706f9b006dfb5f9bbcbb
Tobe-Wang <hzbeq@qq.com>
2025-08-04 07:33:36 +0800

    refactor(FileController): 移除文件上传和下载接口中的参数校验
    - 删除了 upload、uploadMultiple 和 download 方法中的参数校验逻辑
    - 移除了 HttpException 相关的导入语句- 优化了代码结构，提高了代码的可读性和简洁性

--- src/main/java/cn/zhaofd/coretestweb/demo/web/FileController.java
+++ src/main/java/cn/zhaofd/coretestweb/demo/web/FileController.java
@@ -4,13 +4,10 @@
 
 package cn.zhaofd.coretestweb.demo.web;
 
-import cn.zhaofd.core.base.StringUtil;
-import cn.zhaofd.core.net.exception.HttpException;
 import cn.zhaofd.core.spring.web.RestFileUtil;
 import cn.zhaofd.coretestweb.config.PropertyConfig;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -43,11 +40,6 @@ public class FileController {
      */
     @RequestMapping(value = "/upload", method = RequestMethod.POST, produces = MediaType.TEXT_PLAIN_VALUE)
     public String upload(@RequestParam MultipartFile file, HttpServletRequest request) {
-        // 输入参数验证
-        if (file == null || file.isEmpty()) {
-            throw new HttpException(HttpStatus.BAD_REQUEST.value(), "文件为空");
-        }
-
         /*
         // 上传文件(使用应用相对目录保存)
         String dirPath = RestFileUtil.getRealPath(request, propertyConfig.getValue("server.upload.relativeDir"));
@@ -60,17 +52,11 @@ public class FileController {
     /**
      * 上传多个文件
      *
-     * @param files   文件数组
-     * @param request HttpServletRequest对象
+     * @param files 文件数组
      * @return 状态码
      */
     @RequestMapping(value = "/upload/multiple", method = RequestMethod.POST, produces = MediaType.TEXT_PLAIN_VALUE)
-    public List<String> uploadMultiple(@RequestParam MultipartFile[] files, HttpServletRequest request) {
-        // 输入参数验证
-        if (files == null || files.length == 0) {
-            throw new HttpException(HttpStatus.BAD_REQUEST.value(), "文件为空");
-        }
-
+    public List<String> uploadMultiple(@RequestParam MultipartFile[] files) {
         List<String> listResult = new ArrayList<>();
         String uploadDirPath = propertyConfig.getValue("server.upload.dirPath");
         for (MultipartFile file : files) {
@@ -88,14 +74,6 @@ public class FileController {
      */
     @GetMapping(value = "/download")
     public ResponseEntity<byte[]> download(@RequestParam String serverFileName, @RequestParam String downloadFileName) {
-        // 输入参数验证
-        if (StringUtil.isNullOrTrimEmpty(serverFileName)) {
-            throw new HttpException(HttpStatus.BAD_REQUEST.value(), "参数serverFileName不能为空");
-        }
-        if (StringUtil.isNullOrTrimEmpty(downloadFileName)) {
-            throw new HttpException(HttpStatus.BAD_REQUEST.value(), "参数downloadFileName不能为空");
-        }
-
         Path filePath = Paths.get(propertyConfig.getValue("server.upload.dirPath"), serverFileName);
         return RestFileUtil.download(filePath.toAbsolutePath().toString(), downloadFileName);
     }
