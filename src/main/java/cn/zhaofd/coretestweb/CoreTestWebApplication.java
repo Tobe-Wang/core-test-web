97127ac9d5cb6fd448a1ad01257ea8d7c21ef0d3
Tobe-Wang <hzbeq@qq.com>
2025-08-03 13:25:07 +0800

    build:移除数据源和 JPA 相关的自动配置
    - 在 application.yml 中添加 autoconfigure.exclude 配置项，排除以下自动配置：
      - DataSourceAutoConfiguration
      - HibernateJpaAutoConfiguration
      - JpaRepositoriesAutoConfiguration
    - 从 CoreTestWebApplication 类中移除 @SpringBootApplication 注解的 exclude 属性
    - 更新代码注释，推荐使用 YAML 配置的方式进行自动配置排除

--- src/main/java/cn/zhaofd/coretestweb/CoreTestWebApplication.java
+++ src/main/java/cn/zhaofd/coretestweb/CoreTestWebApplication.java
@@ -6,15 +6,13 @@ package cn.zhaofd.coretestweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 /**
  * 启动类
- * <br />exclude=关闭特定的自动配置
+ * <br />exclude=关闭特定的自动配置(推荐方法：通过yaml的spring.autoconfigure.exclude配置)
  */
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, JpaRepositoriesAutoConfiguration.class})
+@SpringBootApplication
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, JpaRepositoriesAutoConfiguration.class})
 public class CoreTestWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(CoreTestWebApplication.class, args);
