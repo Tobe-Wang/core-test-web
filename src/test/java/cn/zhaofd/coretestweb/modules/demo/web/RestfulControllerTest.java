2c83ab664556b20ab98fe53d70fef4b03b2af15b
Tobe-Wang <hzbeq@qq.com>
2025-08-19 17:04:22 +0800

    refactor(test): 更新测试文件路径和包名
    - 将 RestfulControllerTest.java 从 demo包移动到 modules/demo 包- 更新文件路径和包名以适应项目结构的重构

--- src/test/java/cn/zhaofd/coretestweb/modules/demo/web/RestfulControllerTest.java
+++ src/test/java/cn/zhaofd/coretestweb/modules/demo/web/RestfulControllerTest.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2025. Tobe Wang
  */
 
-package cn.zhaofd.coretestweb.demo.web;
+package cn.zhaofd.coretestweb.modules.demo.web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
